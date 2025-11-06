8f4f36c8edce33b2ae2696cde7e41be962ddb480
alamdilshad87 <dilshadand@gmail.com>
2025-11-06 15:36:54 +0530

    Refactor Code Using Methods

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 public class EmployeeWage
-
 {
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
     public const int NUM_OF_WORKING_DAYS = 2;
     public const int MAX_HRS_IN_MONTH = 10;
-    public static void Main(string[] args)
+    public static int computeEmpWage()
     {
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
@@ -33,5 +32,10 @@ public class EmployeeWage
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+        return totalEmpWage;
+    }
+    static void Main(string[] args)
+    {
+        computeEmpWage();
     }
 }
